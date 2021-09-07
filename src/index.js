c5751740a4b1e8a8ef2d4c8ad51de165969ec525
Scott-Kinzer <zaz765831@gmail.com>
2021-09-07 23:09:14 +0300

    upd1.0

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 import './index.css';
-import {renderEntireTree} from "./render";
 import state from "./redux/state";
 import {addPost, changeText} from "./redux/state";
-renderEntireTree(state, addPost, changeText);
+import App from "./App";
+import ReactDOM from 'react-dom';
+import React from "react";
+
+ let renderEntireTree = (state, callbackFunc , secondFunc) => {
+    ReactDOM.render(
+        <React.StrictMode>
+            <App  state={state}  addPost={callbackFunc} changeText={secondFunc}/>
+        </React.StrictMode>,
+        document.getElementById('root')
+    );
+}
+
+renderEntireTree(state, addPost, changeText)
+
+
+
+
 
